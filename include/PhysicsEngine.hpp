026ef10513358918f938a7888b6c010edd8b6b73
arleth murillo velazquez <a21110322@live.ceti.mx>
2024-06-04 18:02:56 -0600

    3

--- include/PhysicsEngine.hpp
+++
@@ -1,8 +0,0 @@
-//Esta clase podría ser responsable de actualizar la posición y la velocidad del jugador en función de la física del juego.
-#pragma once
-#include <fstream>
-#include <Player.hpp>
-#include <Platform.hpp>
-#include <vector>
-using namespace std;
-using namespace ftxui;
--- include/Rendere.hpp
+++
@@ -1,8 +0,0 @@
-//Esta clase podría ser responsable de dibujar el estado del juego en la pantalla.
-#pragma once
-#include <fstream>
-#include <Player.hpp>
-#include <Platform.hpp>
-#include <vector>
-using namespace std;
-using namespace ftxui;
