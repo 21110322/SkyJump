d5530bb89e4035d0ad301ba422e8610a26b0cb21
arleth murillo velazquez <a21110322@live.ceti.mx>
2024-06-06 19:54:09 -0600

    06/06/24

---
+++ src/06_Primitivas.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        // Bloque de brinco
+      
+        sf::RectangleShape rectangle(sf::Vector2f(200, 100));
+        rectangle.setFillColor(sf::Color(185,122,87));
+        rectangle.setPosition(300, 200);
+        window.draw(rectangle);
+
+  
+        window.display();
+    }
+
+    return 0;
+}
