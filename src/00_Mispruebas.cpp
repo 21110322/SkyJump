30fbe2dcb3c57971070e0ae211efd7b6253db675
arleth murillo velazquez <a21110322@live.ceti.mx>
2024-06-14 18:06:12 -0600

    14/06/24

--- src/00_Mispruebas.cpp
+++ src/00_Mispruebas.cpp
@@ -1,6 +1,6 @@
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <vector>
-
 
 class Personaje
 {
@@ -24,7 +24,6 @@ public:
         window.draw(sprite);
     }
 
-private:
     sf::Texture texture;
     sf::Sprite sprite;
 };
@@ -33,11 +32,29 @@ double velocidad = 190;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(736, 600), "Doodle Jump");
+    sf::RenderWindow window(sf::VideoMode(736, 800), "Doodle Jump");
 
-    Personaje conejo(sf::Vector2f(20, 300), "./assets/images/conejo.png");
+    Personaje conejo(sf::Vector2f(20, 500), "./assets/images/conejo.png");
     Personaje fondo(sf::Vector2f(0, 0), "./assets/images/cielo.jpg");
-    Personaje bloque(sf::Vector2f(20, 30), "./assets/images/bloque.png");
+    Personaje zanahoria(sf::Vector2f(20, 30), "./assets/images/zanahoria.png");
+    zanahoria.sprite.setScale(0.5f, 0.5f); 
+    Personaje fresa(sf::Vector2f(489, 355), "./assets/images/fresa.png");
+    fresa.sprite.setScale(0.5f, 0.5f); 
+
+
+
+
+    fondo.texture.setRepeated(true);
+
+    sf::Music music;
+    if (!music.openFromFile("./assets/music/musica.ogg"))
+    {
+        return -1;
+    }
+
+    music.play();
+
+    float fondoY = 0;
 
     while (window.isOpen())
     {
@@ -60,22 +77,33 @@ int main()
                 }
             }
         }
-//Inicio del movimiento del fondo PENDIENTE
 
+        window.clear();
 
-    {
-    
-   fondo.move(0, -0.1);
-
-
-    }
+        for (int i=0;i<10;++i)
+        {
+            sf::Sprite spriteFondo = fondo.sprite;
+            spriteFondo.setPosition(0, i * fondo.texture.getSize().y + fondoY);
+            window.draw(spriteFondo);
+        }
 
         window.clear();
         fondo.draw(window);
-        //fondo1.draw(window);
         conejo.draw(window);
-        //bloque.draw(window);
+        zanahoria.draw(window);
+        fresa.draw(window);
         window.display();
+
+        fondoY =-0.1;
+        if (fondoY<=-fondo.texture.getSize().y)
+        {
+            fondoY = 0;
+        }
+
+        if (music.getStatus() != sf::Music::Playing)
+        {
+            window.close();
+        }
     }
 
     return 0;
